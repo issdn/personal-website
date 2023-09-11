8ae9af1d2727c90474997155770b9b58dcd86c23
issdn <isdnblk@gmail.com>
2023-09-11 21:39:32 +0200

    Update translation & dark mode icons, fix minor acessibility & add styling.

--- src/lib/components/DarkmodeSwitch.svelte
+++ src/lib/components/DarkmodeSwitch.svelte
@@ -1,35 +1,45 @@
 <script lang="ts">
-	import DarkMode from '$lib/symbols/DarkMode.svelte';
-	import LightMode from '$lib/symbols/LightMode.svelte';
-	import darkModeStore from './darkmodeStore';
+  import DarkMode from "$lib/symbols/DarkMode.svelte";
+  import LightMode from "$lib/symbols/LightMode.svelte";
+  import darkModeStore from "./darkmodeStore";
 
-	const setDark = () => {
-		document.documentElement.classList.add('dark');
-		darkModeStore.switchMode();
-	};
+  const setDark = () => {
+    document.documentElement.classList.add("dark");
+    darkModeStore.switchMode();
+  };
 
-	const setLight = () => {
-		document.documentElement.classList.remove('dark');
-		darkModeStore.switchMode();
-	};
+  const setLight = () => {
+    document.documentElement.classList.remove("dark");
+    darkModeStore.switchMode();
+  };
 
-	const toggle = () => {
-		$darkModeStore ? setLight() : setDark();
-	};
+  const toggle = () => {
+    $darkModeStore ? setLight() : setDark();
+  };
+
+  let hovering = false;
 </script>
 
 <button
-	aria-label="Toggle dark mode"
-	on:click={toggle}
-	class="dark:[&>path]:fill-light [&>path]:fill-dark relative transition-colors duration-500 bg-accent-secondary dark:bg-accent-secondary-dark flex flex-row py-2 px-2 rounded-2xl gap-x-4 items-center"
+  on:mouseenter={() => (hovering = true)}
+  on:mouseleave={() => (hovering = false)}
+  on:focus={() => (hovering = true)}
+  on:blur={() => (hovering = false)}
+  aria-label="Toggle dark mode"
+  on:click={toggle}
+  class="h-12 w-12 rounded-full relative hover:bg-accent-secondary dark:hover:bg-accent-secondary-dark overflow-hidden"
 >
-	<DarkMode class="h-[20px] fill-primary dark:fill-light"><desc>Turn on dark mode</desc></DarkMode>
-	<LightMode class="h-[20px] fill-primary dark:fill-light"
-		><desc>Turn off dark mode</desc></LightMode
-	>
-	<span
-		class={`absolute rounded-full bg-primary dark:bg-light h-[calc(100%-8px)] aspect-square top-1 left-1.5 transition-transform duration-500 ${
-			$darkModeStore && 'translate-x-[calc(100%+6px)]'
-		}`}
-	/>
+  {#if $darkModeStore}
+    <DarkMode
+      filled={hovering}
+      class="h-6 w-6 absolute fill-primary dark:fill-light left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2"
+      ><desc>Turn on dark mode</desc></DarkMode
+    >
+  {:else}
+    <LightMode
+      filled={hovering}
+      class="h-6 w-6 absolute fill-primary dark:fill-light left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2"
+      ><desc>Turn off dark mode</desc></LightMode
+    >
+  {/if}
 </button>
--- src/lib/components/LanguageDropdown.svelte
+++ src/lib/components/LanguageDropdown.svelte
@@ -1,44 +1,46 @@
 <script lang="ts">
-	import { slide } from 'svelte/transition';
-	import language, { languages, type Languages } from './languageStore';
-	import Language from '$lib/symbols/Language.svelte';
+  import { slide } from "svelte/transition";
+  import language, { languages, type Languages } from "./languageStore";
+  import Language from "$lib/symbols/Language.svelte";
+  import Translate from "$lib/symbols/Translate.svelte";
 
-	let expanded = false;
+  let expanded = false;
 
-	const setLanguage = (lang: Languages) => {
-		language.set(lang);
-		expanded = false;
-	};
+  const setLanguage = (lang: Languages) => {
+    language.set(lang);
+    expanded = false;
+  };
 </script>
 
-<div class={`relative h-full rounded-t-xl`}>
-	<button
-		aria-label="Language Change Dropdown Toggle"
-		on:click={() => (expanded = !expanded)}
-		class="h-full group origin-center transition-transform duration-100"
-		><Language class="fill-primary dark:fill-light w-11 h-11 group-hover:rotate-6 origin-center"
-			><desc>Change language dropdown button</desc></Language
-		></button
-	>
-	{#if expanded}
-		<ul
-			transition:slide={{ duration: 200 }}
-			class="transition-colors duration-500 gap-y-1 mt-1 text-xl absolute w-full flex flex-col items-center bg-light dark:bg-dark pb-2 rounded-b-xl"
-		>
-			{#each languages as lang}
-				<li>
-					<button
-						class={`transition-colors duration-500 px-1 pb-1 rounded-lg leading-6
+<div class={`relative rounded-t-xl`}>
+  <button
+    aria-label="Language Change Dropdown Toggle"
+    on:click={() => (expanded = !expanded)}
+    class="h-12 w-12 flex items-center justify-center hover:bg-accent-secondary dark:hover:bg-accent-secondary-dark rounded-full"
+    ><Translate class="fill-primary dark:fill-light w-6 h-6"
+      ><desc>Change language dropdown button</desc></Translate
+    ></button
+  >
+  {#if expanded}
+    <ul
+      transition:slide={{ duration: 200 }}
+      class="transition-colors duration-500 gap-y-1 mt-1 text-xl absolute w-full flex flex-col items-center bg-light dark:bg-dark pb-2 rounded-b-xl"
+    >
+      {#each languages as lang}
+        <li>
+          <button
+            class={`transition-colors duration-500 px-1 pb-1 rounded-lg leading-6
                         hover:bg-accent-secondary dark:hover:bg-accent-secondary-dark
                         ${
-													lang === $language && 'bg-accent-secondary dark:bg-accent-secondary-dark'
-												}`}
-						on:click={() => setLanguage(lang)}
-					>
-						{lang}</button
-					>
-				</li>
-			{/each}
-		</ul>
-	{/if}
+                          lang === $language &&
+                          "bg-accent-secondary dark:bg-accent-secondary-dark"
+                        }`}
+            on:click={() => setLanguage(lang)}
+          >
+            {lang}</button
+          >
+        </li>
+      {/each}
+    </ul>
+  {/if}
 </div>
