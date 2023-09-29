dc2918b3f3602de60ce0e84f90f95a4a4fb4cbe5
issdn <karol.bielski@gmx.de>
2023-09-29 22:31:33 +0200

    Tiny UI redesign

--- src/lib/album/Album.svelte
+++ src/lib/album/Album.svelte
@@ -4,7 +4,8 @@
   import IMG3 from "$lib/assets/img3.webp?url";
   import IMG4 from "$lib/assets/img4.webp?url";
   import icon from "$lib/assets/icon32.png?url";
-  import Checkout from "$lib/assets/Checkout.gif?url";
+  import Arrow from "$lib/symbols/Arrow.svelte"
+  import Refresh from "$lib/symbols/Refresh.svelte"
 
   import { texts, type TranslationKeys } from "$lib/translation";
 
@@ -46,16 +47,9 @@
         "Svelte, FastAPI, TawilindCSS, Docker Compose, Stylegan3 AI, Websockets",
       github: "guesswho",
     },
-    {
-      name: "Epic Stuff",
-      url: Checkout,
-      stack: "",
-      github: "",
-      descKey: "" as TranslationKeys,
-    },
   ];
-
-  let currIndex = 0;
+    let index = 0
+    $: tab = imageByProject[index]
 </script>
 
 <div
@@ -63,53 +57,40 @@
 >
   <div
     style="scrollbar-gutter: stable;"
-    class="flex flex-row gap-x-2 pb-2 pt-1 px-2 items-center overflow-x-auto overflow-y-hidden"
+    class="flex flex-row gap-x-2 pb-2 pt-1 px-2 items-center tabs-center overflow-x-auto overflow-y-hidden"
   >
     <img alt="Icon" class="h-5" src={icon} />
     {#each imageByProject as { name }, i}
       <button
-        on:click={() => (currIndex = i)}
-        class="bg-light-tint-20 dark:bg-dark-tint-10 px-2 rounded-sm font-primary whitespace-nowrap"
+        on:click={() => (index = i)}
+        class={`${name === tab.name && "bg-light-tint-20 dark:bg-dark-tint-10"} hover:bg-light-tint-20 dark:hover:bg-dark-tint-10 px-2 rounded-sm font-primary whitespace-nowrap`}
         >{name}<b class="font-secondary">&nbsp;x</b></button
       >
     {/each}
   </div>
   <div
-    class="font-arial flex flex-row gap-x-8 bg-light-tint-20 dark:bg-dark-tint-10 py-1 px-4"
+    class="font-arial flex flex-row tabs-center gap-x-4 bg-light-tint-20 dark:bg-dark-tint-10 py-1 px-4"
   >
-    <div class="flex flex-row text-xl">
-      <span>⬅️</span>
-      <span>➡️</span>
-      <span>🔃</span>
+    <div class="flex flex-row text-xl gap-x-1">
+      <button class="h-8 w-8 flex flex-row items-center justify-center hover:bg-white/25 rounded-md fill-primary dark:fill-light" on:click={() => (index = (index + imageByProject.length - 1) % imageByProject.length)}><Arrow class="h-6 w-6"/></button>
+      <button class="h-8 w-8 flex flex-row items-center justify-center hover:bg-white/25 rounded-md fill-primary dark:fill-light" on:click={() => (index = (index + 1) % imageByProject.length)}><Arrow class="h-6 w-6 rotate-180"/></button>
+      <button class="h-8 w-8 flex flex-row items-center justify-center hover:bg-white/25 rounded-md fill-primary dark:fill-light" on:click={() => (index = 0)}><Refresh class="h-6 w-6"/></button>
     </div>
-    <span class="w-full pt-0.5 bg-light dark:bg-dark text-md px-1 truncate">
-      http://www.past.com/{imageByProject[currIndex].name
-        .toLowerCase()
-        .replace(" ", "")}.html
-    </span>
+    <span class="bg-light rounded-sm dark:bg-dark px-2 py-1 w-full">
+      <a target="_blank"
+      class="hover:text-blue-500 hover:underline text-md truncate w-fit"
+      href={`https://github.com/issdn/${tab.github}`} >
+      https://github.com/issdn/{tab.github}
+    </a>
+  </span>
   </div>
   <div
     bind:this={page}
     class="h-full overflow-y-scroll bg-white text-black font-serif p-8 flex flex-col gap-y-4"
   >
-  {#if imageByProject[currIndex].descKey}
-    <h2 class="text-xl font-bold">{imageByProject[currIndex].name}</h2>
-      <p>{$texts[imageByProject[currIndex].descKey]}</p>
-      <p>Stack: {imageByProject[currIndex].stack}</p>
-      <a
-        target="_blank"
-        class="text-blue-600 hover:underline text-blue-500 w-fit"
-        href={`https://github.com/issdn/${imageByProject[currIndex].github}`}
-        >Github Link</a
-      >
-    {/if}
-
-    {#key imageByProject[currIndex].url}
-      <img
-        class="w-fit"
-        alt={imageByProject[currIndex].name}
-        src={imageByProject[currIndex].url}
-      />
-    {/key}
+    <h2 class="text-xl font-bold">{tab.name}</h2>
+    <p>{$texts[tab.descKey]}</p>
+    <p>Stack: {tab.stack}</p>
+    <img class="w-fit" alt={tab.name} src={tab.url} />
   </div>
 </div>
